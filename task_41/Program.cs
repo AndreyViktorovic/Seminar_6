7edfa17789f9edbf3bd92070dc0c4d0046a6acfb
«andrey <zverkov_a_v@mail.ru>
2023-07-02 09:46:15 +0500

    Добавил задачи 6 семинара

---
+++ task_41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int Chislo(string a)
+{
+    Console.WriteLine(a);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int[] GetRandomArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Chislo($"Введите {i + 1} элемент");
+    }
+    return array;
+}
+
+void ReverseArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]} ");
+    }
+}
+int ChisloNum(int[] array)
+{
+    int b = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            b++;
+        }
+    }
+    return b;
+}
+int length = Chislo("Введите количество элементов ->  ");
+int[] array;
+array = GetRandomArray(length);
+ReverseArray(array);
+Console.WriteLine($"Количество чисел больше 0 ->{ChisloNum(array)} ");
---
+++ task_43/Program.cs
@@ -0,0 +1,60 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines (lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.WriteLine($"Точка пересечения уравнений y={lineData1[CONSTANT]}*x+{lineData1[COEFFICIENT]} и y={lineData2[CONSTANT]}*x+{lineData2[COEFFICIENT]} ");
+    Console.WriteLine($" имеет координаты ({coord[X_COORD]}, {coord[Y_COORD]})");
+}
+
+double Chislo(string a)
+{
+    Console.WriteLine(a);
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[]lineData = new double[2];
+    lineData[COEFFICIENT] = Chislo($"Введите b {numberOfLine} >");
+    lineData[CONSTANT] = Chislo($"Введите k {numberOfLine} >");
+    return lineData;
+}
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[Y_COORD] = lineData1[COEFFICIENT] * coord[X_COORD] + lineData1[CONSTANT];
+    return coord;
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if(lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
+    {
+        if(lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("Прямые совпадают ");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые паралельны ");
+            return false;
+        }
+    }
+    return true;
+}
